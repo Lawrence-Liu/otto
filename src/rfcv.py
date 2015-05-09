8c96dc0973be869a16d9855fd55b03d9d6304afa
Lawrence Liu <suo.lawrence.liu@gmail.com>
2015-05-09 00:52:31 -0400

    adjusted for instance

--- src/rfcv.py
+++ src/rfcv.py
@@ -3,13 +3,13 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.grid_search import GridSearchCV
 
-op = OttoProject("/Users/lawrence/Dropbox/kaggle/otto/data/")
+op = OttoProject("../data/train.csv")
 op.load_original()
 #range_n_estimators = [100,300, 900, 1500]
 #range_max_features = [3, 10, 30]
-range_max_depth = [2,3,4]
-params = [{"n_estimators":range_n_estimators, "max_features":range_max_features, "max_depth":range_max_depth}]
-rf = RFC(n_estimators = 100)
+range_max_depth = [5,10,20, 30]
+params = [{"max_depth":range_max_depth}]
+rf = RFC(n_estimators = 500)
 clf = GridSearchCV(rf, params, n_jobs = -1, verbose = 2, scoring = 'log_loss')
 clf.fit(op.subtrain_features, op.subtrain_classes)
 print str(clf.best_score_)+"\n"

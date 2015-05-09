2d2c8890bb8d90afdb91557dfaaf5a86c9ed182a
Lawrence Liu <suo.lawrence.liu@gmail.com>
2015-05-09 01:01:29 -0400

    grid search for n_estimators, max_features and max_depth

--- src/rfcv.py
+++ src/rfcv.py
@@ -5,11 +5,11 @@ from sklearn.grid_search import GridSearchCV
 
 op = OttoProject("../data/")
 op.load_original()
-#range_n_estimators = [100,300, 900, 1500]
-#range_max_features = [3, 10, 30]
-range_max_depth = [5,10,20, 30]
-params = [{"max_depth":range_max_depth}]
-rf = RFC(n_estimators = 500)
+range_n_estimators = [100, 300, 900, 1500]
+range_max_features = [3, 10, 30 , 50]
+range_max_depth = [5, 15,30, 45]
+params = [{"max_depth":range_max_depth, "max_features":range_max_features, "n_estimators":range_n_estimators}]
+rf = RFC()
 clf = GridSearchCV(rf, params, n_jobs = -1, verbose = 2, scoring = 'log_loss')
 clf.fit(op.subtrain_features, op.subtrain_classes)
 print str(clf.best_score_)+"\n"
